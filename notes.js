5a1a7a92b8c15a0718a34e22dbe925e1aa8deb4d
josh119891 <josh119891@hotmail.com>
2019-11-06 17:38:09 +0800

    NOV-6

--- notes.js
+++ notes.js
@@ -4,8 +4,8 @@ function resolveAfterSeconds(x) {
       setTimeout(() => {
         resolve(x);
       }, 1500);
-    });
   }
+}
   
   async function f1() {
     var x = await resolveAfterSeconds(10);
@@ -15,9 +15,15 @@ function resolveAfterSeconds(x) {
   f1();
 `
   oct-31
-  1.
-  await只是返回一个proise，但若想用await返回具体数值
-  promise里面要写resolve
-  2.https://www.jianshu.com/p/f0583f17e2ca
-  3.https://www.cnblogs.com/karthuslorin/p/8577224.html
+  1.async/await
+      await只是返回一个promise，但若想得到具体数值,promise里要写resolve(value)
+
+  nov-5
+  1.Vue
+      怎么使用Vue(刷视频和实操COMP)
+  2.学习资源
+      https://cnodejs.org/topic/56ef3edd532839c33a99d00e
+  3.Bing API接口
+      https://blog.csdn.net/xuaho0907/article/details/74393010
+
 `
