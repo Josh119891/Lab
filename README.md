8b4883f8b9f7073440e82ad4385681a318cc0b25
Josh119891 <Josh_Li@epam.com>
2022-03-23 22:25:22 +0800

    更新

---
+++ README.md
@@ -0,0 +1,10 @@
+# 笔记
+
+### 2020进展
+-[x] AWS developer associate 证书
+-[] React Router
+-[] interview
+
+
+### 文档
+1. [DVA笔记](https://1drv.ms/u/s!AiR30pWX0ETziwgj-eYtQYcDC8y7)
--- online.js
+++ online.js
@@ -1,4 +1,13 @@
 
-
+// TOOLS
 // A beautify online road-map maker
 const MIRO = 'https://miro.com/';
+
+
+
+
+
+
+// PERSONAL NOTES
+// My notes for AWS DVA exam
+const DVA_NOTEBOOK = 'https://1drv.ms/u/s!AiR30pWX0ETziwgj-eYtQYcDC8y7';
