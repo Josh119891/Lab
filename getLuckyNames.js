3a9e703b938ff319723c48274f4e4c27c831b134
Josh119891 <josh119891@hotmail.com>
2025-05-16 16:16:42 +0800

    标准化，更改格式

--- getLuckyNames.js
+++ getLuckyNames.js
@@ -1,30 +1,40 @@
 
 
-const waterStrokes = {
-  "凡": 3, "水": 4, "汀": 7, "江": 7, "河": 8, "汐": 7, "沁": 8, "清": 12, "霏": 16,
-  "洋": 10, "洲": 9, "洁": 10, "洪": 9, "海": 11,
-  "浪": 13, "涵": 12, "涛": 18, "润": 16,
-  "淇": 11, "淼": 12, "渊": 12, "渤": 14,
-  "渐": 12,
-  "滔": 15, "滢": 15,
-  "漾": 14, "潇": 20, "潭": 15,
-  "潮": 17, "澜": 16, "澈": 16, "澎": 17,
-  "澹": 16, "沐": 8, "溪": 14, "洛": 10, "霖": 16,
-  "慧": 15, "雯": 12, "雨": 8, "冰": 6,
-  "沛": 8, "沫": 9,
+
+const WATER_STROKES = {
+  "凡": 3, "水": 4, "冰": 6, "汀": 7, "江": 7, "汐": 7, "河": 8,
+  "沁": 8, "沐": 8, "雨": 8, "沛": 8, "沫": 9, "洲": 9, "洪": 9,
+  "洋": 10, "洁": 10, "洛": 10, "海": 11, "淇": 11, "淼": 12, "涵": 12,
+  "雯": 12, "渊": 12, "渐": 12, "清": 12, "淑": 12,
+  "浪": 13, "熙": 13, "渤": 14, "漾": 14, "溪": 14, "滔": 15, "潭": 15, "滢": 15,
+  "慧": 15, "润": 16, "澜": 16, "澈": 16, "霏": 16, "澹": 16, "霖": 16, "澄": 16, "浔": 16,
+  "潮": 17, "澎": 17,
+  "涛": 18, "潇": 20, "潇": 20,
 };
 
-const woodStrokes = {
-
-  "可": 5, "兰": 8, "卉": 9, "林": 8, "柏": 9, "柳": 9, "桃": 10, "苒": 11,
-  "枫": 12, "树": 12, "杨": 13, "楠": 13, "榆": 13, "楚": 15, "桥": 16,
-  "檬": 16, "檩": 17, "柠": 18,
-  "若": 11, "棋": 12, "樱": 21,
-  "芊": 9, "茜": 12, "茵": 12, "茹": 12, "幂": 12,
-  "柔": 9, "芷": 10, "枚": 8, "枝": 8, "青": 8, "冉": 5, "檬": 18, "槿": 15, "萱": 15, "茉": 11, "蓉": 16, "萌": 14, "芝": 10, "莘": 13, "蔚": 17, "筱": 13, "艺": 21, "荔": 12,
+const WOOD_STROKES = {
+  "可": 5, "冉": 5,
+  "彤": 7,
+  "兰": 8, "林": 8, "枚": 8, "枝": 8, "青": 8,
+  "芊": 9, "柔": 9, "卉": 9, "柏": 9, "柳": 9, "榕": 9, "柯": 9,
+  "桃": 10, "芷": 10, "芸": 10, "芮": 10, "芝": 10,
+  "苒": 11, "苑": 11, "若": 11, "苗": 11, "茉": 11,
+  "枫": 12, "树": 12, "雅": 12, "棋": 12, "茜": 12, "茵": 12, "茹": 12, "幂": 12, "荔": 12,
+  "杨": 13, "楠": 13, "榆": 13, "莉": 13, "莘": 13, "筱": 13,
+  "嘉": 14, "梦": 14, "萌": 14,
+  "楚": 15, "慕": 15, "槿": 15, "萱": 15,
+  "桥": 16, "檬": 16, "橙": 16, "柚": 16, "蓉": 16,
+  "檩": 17, "蔚": 17, "柠": 18, "蕊": 18, "檬": 18,
+  "薇": 19, "蕾": 19,
+  "樱": 21, "艺": 21,
 }
 
-const strokesMap = { "李": 7, ...waterStrokes, ...woodStrokes }
+
+//这里是姓氏,姓在这里单独添加，不用做名
+const SURNAME = { "李": 7, };
+
+const strokesMap = { ...SURNAME, ...WATER_STROKES, ...WOOD_STROKES }
+
 
 function isLuckyGrids(grids) {
   const nums = [1, 3, 5, 6, 8, 11, 13, 15, 16, 18, 21, 23, 24, 25, 31, 32, 33, 37, 39, 41];
@@ -67,10 +77,10 @@ function isLuckyTalents(tian, ren, di) {
   else if (t2r === '平' && r2d === '平') luck = '平';
   else luck = '凶';
 
-  return { tian, ren, di, tianE, renE, diE, relation: { t2r, r2d }, luck };
+  return luck;
 }
 
-function isLuckyName(name) {
+function getFiveGrids(name) {
   if (name.length < 2 || name.length > 3) {
     console.log('⚠️ 名字应为2~3个字（含姓）。');
     return;
@@ -90,28 +100,61 @@ function isLuckyName(name) {
   const waige = (typeof tiange === 'number' && typeof dige === 'number' && typeof renge === 'number')
     ? tiange + dige - renge : '?';
   const zongge = totalStrokes;
-  const fiveGrids = [tiange, renge, dige, waige, zongge];
-  return isLuckyGrids(fiveGrids) && isLuckyTalents(tiange, renge, dige).luck !== '凶'
+  return [tiange, renge, dige, waige, zongge];
 }
 
-
-
-function getLuckyNames() {
-  const surname = '李';
-  const result = []
-  for (let woodKey of Object.keys(woodStrokes)) {
-    for (let waterKey of Object.keys(waterStrokes)) {
-      let tempA = surname + woodKey + waterKey;
-      if (isLuckyName(tempA)) {
-        result.push(tempA)
-      }
-      let tempB = surname + waterKey + woodKey
-      if (isLuckyName(tempB)) {
-        result.push(tempB)
-      }
+//
+const A = []; // 大吉
+const B = []; // 吉
+const C = []; // 平
+function judgeName(name) {
+  const [tiange, renge, dige, waige, zongge] = getFiveGrids(name);
+  //要求五格全吉，如果外格允许凶，去掉waige即可
+  if (isLuckyGrids([tiange, renge, dige, waige, zongge])) {
+    const luck = isLuckyTalents(tiange, renge, dige)
+    if (luck === "大吉") {
+      A.push(name)
+    }
+    if (luck === "吉") {
+      B.push(name)
+    }
+    if (luck === "平") {
+      C.push(name);
     }
   }
-  return result;
+  return;
 }
-const names = getLuckyNames()
-console.log(names)
+
+// 起名逻辑
+function getLuckyNames() {
+  const surname = SURNAME.keys[0];
+  //
+  // 这里考虑五行，要的逻辑自己加
+  // 去重
+  const waterChars = [...new Set(Object.keys(WATER_STROKES))];
+  const woodChars = [...new Set(Object.keys(WOOD_STROKES))];
+
+  for (let i = 0; i < waterChars.length; i++) {
+    for (let woodChar of woodChars) {
+      //1木1水
+      let nameA = surname + woodChar + waterChars[i];
+      judgeName(result, nameA);
+      //1水1木
+      let nameB = surname + waterChars[i] + woodChar;
+      judgeName(result, nameB)
+    }
+    //考虑叠词，考虑两个水
+    for (let j = i; j < waterChars.length; j++) {
+      let nameC = surname + waterChars[i] + waterChars[j];
+      judgeName(result, nameC)
+
+    }
+  }
+  //
+  //
+  return;
+}
+
+console.log(A);
+console.log(B);
+console.log(C);
